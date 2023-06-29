1d6d353436e7b4c2fc9683a23c2026ea52430257
keshridivya <dkeshari094@gmail.com>
2023-06-29 10:33:09 +0530

    product show

--- application/controllers/frontend.php
+++ application/controllers/frontend.php
@@ -4,11 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Frontend extends CI_Controller {
 	public function index()
 	{
-		$this->db->select('*, product.id as pid');
+		$this->db->select('*, product.id as pid,price-(price*dis_price/100) as rate');
 		$this->db->from('category');
 		$this->db->join('product', 'category.id = product.category', 'right');
 		$page_data['product'] = $this->db->get()->result_array();
 
 		$this->load->view('front/index',$page_data);
+	}
+
+	public function productThumbnail($id=false)
+	{
+		$this->db->select('*, product.id as pid,price-(price*dis_price/100) as rate');
+		$this->db->from('category');
+		$this->db->join('product', 'category.id = product.category', 'right');
+		$this->db->where('product.id',$id);
+		$page_data['product'] = $this->db->get()->row();
+		// $page_data['product_img'] = $this->db->get_where('product',array('id'=>$id))->result_array();
+
+		$this->load->view('front/product-thumbnail',$page_data);
 	}
 }
--- application/views/front/include/head.php
+++ application/views/front/include/head.php
@@ -41,4 +41,5 @@
 
     <!-- Template css -->
     <link id="color-link" rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"  />
 </head>
